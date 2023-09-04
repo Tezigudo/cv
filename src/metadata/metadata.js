f14e221a711a1f8b7d4919118a62b2b62f128862
Preawpan Thamapipol <80502090+Tezigudo@users.noreply.github.com>
2023-09-04 22:43:14 +0700

    add more information

--- src/metadata/metadata.js
+++ src/metadata/metadata.js
@@ -5,60 +5,65 @@ module.exports = {
     'LinkedIn': '<a href="https://www.linkedin.com/in/preawpan-thamapipol-0b1b1a1a0/" target="_blank"><i class="fa fa-linkedin fact-icon"></i>preawpan-thamapipol</a>',
     'Email': '<a href="mailto:godjangg+contract@gmail.com"><i class="fa fa-envelope fact-icon"></i>godjangg+contract@gmail.com</a>',
   },
-  skills: [
-    ['Comedy', 100],
-    ['Love', 100],
-    ['Theater', 100],
-    ['Tragedy', 100],
-    ['Poetry', 100],
-    ['Erotic', 75],
-    ['Sonnets', 100],
-    ['Plays', 100],
-    ['Writing', 100],
-    ['Directing', 75],
-    ['Literature', 100],
-    ['Influence', 100],
-  ],
+  // skills: [
+  //   ['Comedy', 100],
+  //   ['Love', 100],
+  //   ['Theater', 100],
+  //   ['Tragedy', 100],
+  //   ['Poetry', 100],
+  //   ['Erotic', 75],
+  //   ['Sonnets', 100],
+  //   ['Plays', 100],
+  //   ['Writing', 100],
+  //   ['Directing', 75],
+  //   ['Literature', 100],
+  //   ['Influence', 100],
+  // ],
+  skills: {
+    'Computer Languages': 'Python, Java, C++, JavaScript, SQL, Bash',
+    'Frameworks': 'Django, Spring Boot, Robot Framework, Numpy, Pandas, Scrum, Omise, FastAPI',
+    'Tools': 'Git, Selenium, Markdown, Jupyter Notebook, VS Code, pip, Vim, IntelliJ IDEA, Airflow',
+    'Others': 'Heroku, SQLite, GitHub Action, Tkinter, MongoDB, RabbitMQ',
+    'Languages': 'English (Upper Intermediate), Thai (Native), Japanese (Beginner)'
+  },
   positions: [
     {
-      title: 'Hamlet',
-      period: 'Likely early 17th century',
-      skills: ['tragedy', 'philosophy', 'death'],
-      contents: `
-Prince Hamlet is visited by his father's ghost and ordered to avenge his father's murder by killing King Claudius, his uncle. After struggling with several questions, including whether what the ghost said is true and whether it is right for him to take revenge, Hamlet, along with almost all the other major characters, is killed.
-`
+      title: '<a class="link-dark text-decoration-underline" href="https://drive.google.com/file/d/1FAaos9M6f7YlF6dLXx0XW7vIq7MX-O_T/view?usp=share_link" target="_blank">Software Engineer, Backend Intern(Java)</a>, <a class="link-dark text-decoration-underline" href="https://lmwn.com" target="_blank">LINEMAN Wongnai</a>',
+      period: 'MAY 2023 - JUL 2023',
+      skills: ['Spring Boot', 'Scrum', 'Github Flow'],
+      contents: [`Develop Wongnai and Merchant App by improving the system and adding new features, and collaborate with Frontend/Backend/iOS/Android teams to design the system functionality.`, `Improve code quality to make the code efficient, functional, and easy to run test and maintain.`, `Support on documents creating system designing such as ​​Sequence Diagram, API Spec, Collaboration Diagram and etc.`]
     },
     {
       title: 'Twelfth Night',
       period: '1600 – 1601',
       skills: ['comedy', 'love'],
-      contents: `
+      contents: [`
 Viola finds herself shipwrecked in Illyria and, assuming that her brother Sebastian has died in the wreck, disguises herself as a man to gain a position in Duke Orsino's court. Orsino sends Viola (whom he knows as Cesario) to deliver a message to his love, Olivia. Olivia, however, dislikes the Duke. She falls in love with Viola, who she thinks is a man. Eventually, Viola's brother Sebastian, who in fact was unharmed in the wreck, reappears. At a critical moment, Viola's true identity is revealed when members of the court notice the similarities between her and Sebastian. Olivia quickly falls in love with Sebastian, and Viola confesses her love for the Duke.
-`
+`]
     },
     {
       title: 'The Tragedy of Romeo and Juliet',
       period: '1595–1596, with a possible early draft written in 1591',
       skills: ['tragedy', 'death', 'love'],
-      contents: `
+      contents: [`
 In Verona, Italy, two families, the Montagues and the Capulets, are in the midst of a bloody feud. Romeo, a Montague, and Juliet, a Capulet, fall in love and struggle to maintain their relationship in the face of familial hatred. After Romeo kills Juliet's cousin Tybalt in a fit of passion, things fall apart. Both lovers eventually commit suicide within minutes of each other, and the feuding families make peace over their recent grief.
-`
+`]
     }
   ],
   experience: [
     {
       header: 'Performance',
       skills: ['theater', 'play'],
-      contents: `
+      contents: [`
 Thousands of performances of my plays have been staged since the end of the 16th century. Many of my greatest plays were performed by the Lord Chamberlain's Men and King's Men acting companies at the [Globe](https://en.wikipedia.org/wiki/Globe_Theatre) and Blackfriars Theatres.
-`
+`]
     },
     {
       header: 'Poetry',
       skills: ['poetry', 'erotic'],
-      contents: `
+      contents: [`
 In 1593 and 1594, when the theatres were closed because of plague, I published two narrative poems on sexual themes, Venus and Adonis and The Rape of Lucrece. I dedicated them to Henry Wriothesley, Earl of Southampton.
-`
+`]
     }
   ]
 };
